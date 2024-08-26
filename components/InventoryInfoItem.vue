0379d40297d0dfc23e9af87558ed461470c4afd5
AbderrahmenMhemed <abderrahmen.mhemed@wevioo.com>
2024-08-26 21:43:34 +0100

    add twitch auth

--- components/InventoryInfoItem.vue
+++ components/InventoryInfoItem.vue
@@ -10,15 +10,15 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="flex flex-row gap-4 bg-crust rounded-lg p-4 items-center">
-    <div class="p-2 rounded-full bg-base text-base">
+  <div class="flex flex-row gap-4 bg-base rounded-lg p-4 items-center">
+    <div class="p-2 rounded-full bg-crust text-base">
       <img :src="icon" height="32" width="32" :alt="iconAlt" />
     </div>
     <div class="flex flex-col gap-1">
-      <p class="text-base capitalize font-bold">{{ topText }}</p>
+      <p class="text-gray capitalize font-bold">{{ topText }}</p>
       <div class="flex flex-row gap-1">
         <img :src="iconSecondary" v-if="iconSecondary" height="24" width="24" :alt="iconSecondaryAlt" />
-        <p class="text-base">{{ bottomText }}</p>
+        <p class="text-slate-50">{{ bottomText }}</p>
       </div>
     </div>
   </div>
