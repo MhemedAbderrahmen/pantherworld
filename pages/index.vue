1298f92cc14f4b44df0992ee21500387144a59b1
Abderrahmen Mhemed <mhemedabderrahmen@gmail.com>
2024-08-26 15:38:12 +0200

    dark and light mode

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
 const config = useRuntimeConfig();
+const colorMode = useColorMode();
 
 useSeoMeta({
   title: "Welcome to p4nth3rworld",
   description: "The game doesn't stop when the stream ends.",
 });
 
-const { status, data, error, refresh } = useFetch<MetaResponse | null>(`meta/`, {
-  baseURL: config.public.world_api_url,
-  lazy: true,
-});
+const { status, data, error, refresh } = useFetch<MetaResponse | null>(
+  `meta/`,
+  {
+    baseURL: config.public.world_api_url,
+    lazy: true,
+  }
+);
 
 onMounted(() => {
   document.addEventListener("visibilitychange", () => {
@@ -41,20 +45,45 @@ const locationBgClass: locationKeyValue = {
       <span>🌳</span>
       <span>🗺️</span>
     </div>
-    <h1 class="text-4xl mb-6 text-center font-bold max-w-md">The game doesn't stop when the stream ends</h1>
+    <h1 class="text-4xl mb-6 text-center font-bold max-w-md">
+      The game doesn't stop when the stream ends
+    </h1>
     <a
       href="https://twitch.tv/whitep4nth3r/chat"
       class="bg-violet-700 hover:bg-violet-500 focus:bg-emerald-700 active:bg-emerald-700 focus:outline-none focus:ring focus:ring-emerald-300 text-slate-50 py-2 px-4 rounded-lg text-xl transition font-display"
       >play now</a
     >
+    <h1>Color mode: {{ $colorMode.value }}</h1>
+    <div>
+      <select v-model="$colorMode.preference">
+        <option value="system">System</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+        <option value="sepia">Sepia</option>
+      </select>
+    </div>
   </section>
 
   <section v-auto-animate>
-    <div class="mb-16 grid grid-cols-1 sm:grid-cols-2 gap-8 justify-between items-start" v-if="data !== null">
-      <PlayerCard title="Latest claim" :player="data.meta.latestClaim.player" :item="data.meta.latestClaim.item" />
-      <PlayerCard title="Latest cook" :player="data.meta.latestCook.player" :item="data.meta.latestCook.item" />
+    <div
+      class="mb-16 grid grid-cols-1 sm:grid-cols-2 gap-8 justify-between items-start"
+      v-if="data !== null"
+    >
+      <PlayerCard
+        title="Latest claim"
+        :player="data.meta.latestClaim.player"
+        :item="data.meta.latestClaim.item"
+      />
+      <PlayerCard
+        title="Latest cook"
+        :player="data.meta.latestCook.player"
+        :item="data.meta.latestCook.item"
+      />
     </div>
-    <div class="mb-16 grid grid-cols-1 sm:grid-cols-2 gap-8 justify-between items-start" v-else>
+    <div
+      class="mb-16 grid grid-cols-1 sm:grid-cols-2 gap-8 justify-between items-start"
+      v-else
+    >
       <div>
         <USkeleton class="mb-2 w-full h-8" />
         <USkeleton class="w-full h-24" />
@@ -66,27 +95,45 @@ const locationBgClass: locationKeyValue = {
     </div>
   </section>
   <section v-auto-animate>
-    <div class="mb-16 grid grid-cols-1 sm:grid-cols-3 gap-3 my-4" v-if="data !== null">
+    <div
+      class="mb-16 grid grid-cols-1 sm:grid-cols-3 gap-3 my-4"
+      v-if="data !== null"
+    >
       <div class="flex flex-col justify-between">
-        <p class="font-bold uppercase text-lg mb-4 bg-yellow-500 text-zinc-900 text-center rounded-lg">Latest Spawn</p>
+        <p
+          class="font-bold uppercase text-lg mb-4 bg-yellow-500 text-zinc-900 text-center rounded-lg"
+        >
+          Latest Spawn
+        </p>
         <InventoryItem
           :name="data.meta.latestSpawned.name"
           :rarity="data.meta.latestSpawned.rarity"
-          :extraInfo="data.meta.latestSpawned.zone" />
+          :extraInfo="data.meta.latestSpawned.zone"
+        />
       </div>
       <div class="flex flex-col justify-between">
-        <p class="font-bold uppercase text-lg mb-4 bg-yellow-500 text-zinc-900 text-center rounded-lg">Most Spawned</p>
+        <p
+          class="font-bold uppercase text-lg mb-4 bg-yellow-500 text-zinc-900 text-center rounded-lg"
+        >
+          Most Spawned
+        </p>
         <InventoryItem
           :name="data.meta.mostSpawned.name"
           :count="data.meta.mostSpawned.number"
-          :rarity="data.meta.mostSpawned.rarity" />
+          :rarity="data.meta.mostSpawned.rarity"
+        />
       </div>
       <div class="flex flex-col justify-between">
-        <p class="font-bold uppercase text-lg mb-4 bg-yellow-500 text-zinc-900 text-center rounded-lg">Least Spawned</p>
+        <p
+          class="font-bold uppercase text-lg mb-4 bg-yellow-500 text-zinc-900 text-center rounded-lg"
+        >
+          Least Spawned
+        </p>
         <InventoryItem
           :name="data.meta.leastSpawned.name"
           :count="data.meta.leastSpawned.number"
-          :rarity="data.meta.leastSpawned.rarity" />
+          :rarity="data.meta.leastSpawned.rarity"
+        />
       </div>
     </div>
     <div class="mb-16 grid grid-cols-1 sm:grid-cols-3 gap-3 my-4" v-else>
@@ -107,7 +154,11 @@ const locationBgClass: locationKeyValue = {
 
   <section class="mb-16" v-auto-animate>
     <div v-if="data !== null">
-      <p class="font-bold uppercase text-xl bg-yellow-500 text-zinc-900 text-center rounded-lg">Current Population</p>
+      <p
+        class="font-bold uppercase text-xl bg-yellow-500 text-zinc-900 text-center rounded-lg"
+      >
+        Current Population
+      </p>
       <div class="grid grid-cols-1 sm:grid-cols-2 gap-3 my-4" v-auto-animate>
         <InventoryInfoItem
           v-for="(count, zone) in data.meta.population"
@@ -119,7 +170,8 @@ const locationBgClass: locationKeyValue = {
           iconSecondaryAlt="people representing population"
           :iconAlt="zone.toString()"
           :topText="zone"
-          :bottomText="`Zone ${count}`" />
+          :bottomText="`Zone ${count}`"
+        />
       </div>
     </div>
     <div v-else>
@@ -144,5 +196,17 @@ const locationBgClass: locationKeyValue = {
   width: max-content;
   margin: auto;
   font-size: clamp(2rem, 4vw, 4rem);
+}
+.light-mode body {
+  background-color: #eff1f5;
+  color:  #11111b
+}
+.dark-mode body {
+  background-color: #091a28;
+  color: #ebf4f1;
+}
+.sepia-mode body {
+  background-color: #f1e7d0;
+  color: #433422;
 }
 </style>
